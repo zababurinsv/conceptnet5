91cc91dd10b73a139ccfe139176578dac6d9db23
Zababurin <s.zababurin.v@gmail.com>
2021-09-29 02:13:02 +0300

    update

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 import app from './server__main_index.mjs'
 
 let port = process.env.PORT || 5837
-app.listen(port ,() =>{
+app.listen(port ,() => {
     console.log('pid: ', process.pid)
     console.log('listening on http://localhost:'+ port);
 });
