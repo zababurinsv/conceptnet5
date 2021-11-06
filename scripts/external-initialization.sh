57f294ffc59ae61871b8fa9aa54087266b7783bc
Zababurin <s.zababurin.v@gmail.com>
2021-11-06 18:50:24 +0300

    update

--- scripts/external-initialization.sh
+++ scripts/external-initialization.sh
@@ -5,5 +5,11 @@ export GOPATH=$(pwd)
 #go get github.com/mdn/dom-examples
 #sleep ${delay}
 #go get github.com/MetaMask/post-message-stream
+#sleep ${delay}
+#go get github.com/wavesplatform/signer
+#sleep ${delay}
+#go get github.com/AnthumChris/fetch-progress-indicators
+#sleep ${delay}
+#go get github.com/GoogleChromeLabs/comlink
 sleep ${delay}
-go get github.com/wavesplatform/signer
+go get github.com/webtorrent/webtorrent
